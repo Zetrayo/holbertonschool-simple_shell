18491154d258bd19d2547887572338c93102510d
sifir-gun <guney.tasdelen74@gmail.com>
2024-08-13 14:42:58 +0200

    add pid.c and shell.h

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+#endif
