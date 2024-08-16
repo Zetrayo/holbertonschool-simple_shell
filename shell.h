6c2b6ff732484656bc022f01dfc1b52293e99211
Zetrayo <josefeliciano.com98@gmail.com>
2024-08-16 15:33:51 +0200

    ADD : added path.c and code to it

--- shell.h
+++ shell.h
@@ -12,22 +12,36 @@
 #include <fcntl.h>
 #include <errno.h>
 
-/** Define a buffer size for command input */
+/* Define a buffer size for command input */
 #define BUFSIZE 1024
 
+/**
+ * struct list_path - Linked list containing PATH directories
+ * @dir: directory in path
+ * @p: pointer to next node
+ */
+typedef struct list_path
+{
+	char *dir;
+	struct list_path *p;
+} list_path;
 
-/** Function Prototypes */
+/* Function Prototypes */
 
 void prompt(void);
-/** Prints the shell prompt */
+/* Prints the shell prompt */
 char *read_command(void);
-/** Reads a command from the user input */
+/* Reads a command from the user input */
 void execute_command(char *cmd);
-/** Executes the given command */
+/* Executes the given command */
 int check_builtin_commands(char *cmd);
-/** Checks and executes built-in commands */
+/* Checks and executes built-in commands */
 void free_resources(char *cmd, char **args);
-/** Frees allocated resources */
+/* Frees allocated resources */
+list_path *linkpath(char *path);
+/* searches for the PATH */
+list_path *add_node_end(list_path **head, char *str);
+/* adds a new node at the end of a list_t list */
 
 extern char **environ;
 
